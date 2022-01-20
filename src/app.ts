24498d3d901bc102b985413f95d63f233b356482
Ishola Janet <janetishola2010@gmail.com>
2022-01-20 09:24:34 +0100

    redirect implementation

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,32 @@
-import express from "express"
+import express, {Request, Response} from "express"
 
 
 const app = express()
 
 app.use(express.json())
 
-app.use(express.urlencoded({extended: true}))
+// app.use(express.urlencoded({extended: true}))
 
-app.get("/", (req, res) => {
-    return res.send("Hello World");
+app.get("/", (req: Request, res: Response) => {
+    return res.json({
+        success: true,
+        name: "Janet"
+    })
 });
 
-app.post("/api/data", (req, res) => {
+//to redirect 
+app.get("/redirect", (req: Request, res: Response) => {
+    return res.redirect("http://example.com")
+
+})
+
+app.post("/api/data", (req: Request, res: Response) => {
     console.log(req.body);
 
     return res.sendStatus(200)
 })
 
-app.all("/api/all", (req, res) => {
+app.all("/api/all", (req: Request, res: Response) => {
     return res.sendStatus(200);
 })
 
