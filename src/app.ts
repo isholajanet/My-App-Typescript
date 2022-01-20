cf9dc8e9f18a29840cbb36332ba47404e0affe10
Ishola Janet <janetishola2010@gmail.com>
2022-01-20 01:49:28 +0100

    start

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from "express"
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true}))
+
+app.get("./", (req, res) => {
+    return res.send("Hello World");
+});
+
+app.post('/api/data', (req, res) => {
+    console.log(req.body);
+
+    return res.sendStatus(200)
+})
+
+app.all("./api/all", (req, res) => {
+    return res.sendStatus(200);
+})
+
+app.listen(3002, () => {
+    console.log("Application listening at http://localhost:3002")
+})
