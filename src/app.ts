7ec87478e7641b2045823e2404d836804b5f2a8d
Ishola Janet <janetishola2010@gmail.com>
2022-01-20 09:16:26 +0100

    api creation

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,17 @@ app.use(express.json())
 
 app.use(express.urlencoded({extended: true}))
 
-app.get("./", (req, res) => {
+app.get("/", (req, res) => {
     return res.send("Hello World");
 });
 
-app.post('/api/data', (req, res) => {
+app.post("/api/data", (req, res) => {
     console.log(req.body);
 
     return res.sendStatus(200)
 })
 
-app.all("./api/all", (req, res) => {
+app.all("/api/all", (req, res) => {
     return res.sendStatus(200);
 })
 
